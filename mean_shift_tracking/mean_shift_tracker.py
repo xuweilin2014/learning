81deb6d402466947002c11e363b6dbf0cd429f63
xuweilin <xuweilin2014@outlook.com>
2021-08-20 21:48:14 +0800

    增加了二维的颜色直方图

--- mean_shift_tracking/mean_shift_tracker.py
+++ mean_shift_tracking/mean_shift_tracker.py
@@ -5,18 +5,35 @@ from numba import jit
 
 """
 实现了 Mean-Shift Tracker 算法，可以对彩色图像以及灰度图像中的目标进行追踪
+颜色直方图可以是一维或者二维：
+    1.如果是一维的话
+        i.如果是灰度图像，就按照定义生成一个标准的直方图，划分成 bins 个小区间
+        ii.如果是彩色 rgb 图像，就生成一个 3 * bins 个小区间的颜色直方图
+    2.如果是二维的话（只支持彩色 rgb 图像）
+        i.就会生成一个 hue-saturation 二维矩阵，用来代表二维颜色直方图
+
 参考了 mean-shift.pdf 里面的算法流程以及下面的 matlab 代码
 https://github.com/mohitkumarahuja/Visual-Tracking-Using-MeanShift
+mean-shift 跟踪算法的理论推导参考了下面网址的文档和视频：
+https://wenku.baidu.com/link?url=ci6YgcElMehkTNBIMWaNTxmK40oxLXuMunJCUJxQLk4Hw7jitgnEmbz7O-0FMReNwFuwIoR3HixeNGLcSZk5deQPiqDwjaif48PAKbc-hIa
+https://www.youtube.com/watch?v=M8B3RZVqgOo&t=3041s
 """
 
 EPSILON = 1.19209e-09
 ITERATION_LIMIT = 200
-BIN_NUM = 8
+BIN_NUM = 15
+
+class ColorHistogram:
+    # 一维的颜色直方图
+    One = 1
+    # 二维的颜色直方图
+    Two = 2
 
 # noinspection PyAttributeOutsideInit,PyUnusedLocal,PyShadowingNames
 class MeanShiftTracker:
 
-    def init(self, frame, rect):
+    def init(self, frame, rect, ch=ColorHistogram.One):
+        self.DIMENSION = ch
         self.rect = np.round(rect)
         x, y, w, h = self.rect
         roi_center = np.round(np.array([x + w / 2, y + h / 2]))
@@ -24,6 +41,7 @@ class MeanShiftTracker:
         self.roi_height = h
         self.bins = BIN_NUM
 
+        # 如果图像的 shape 只有 (h,w)，即灰度图像，那么将灰度图像 shape 转换成 (h,w,1)
         if len(frame.shape) == 2:
             frame = np.reshape(frame, frame.shape + (1,))
 
@@ -127,15 +145,25 @@ class MeanShiftTracker:
         ratio[p_current <= EPSILON] = 0
         weights = np.zeros((h, w))
 
+        # 分开同样是方便 @jit 对代码进行加速
         # 1.find weights over all the patch, for every pixels
         # 2.check in which bin is the current pixel value
         # 3.compute the weight
-        for k in range(c):
-            indices = (np.divide(img_patch[:,:,k], 255) * bins).astype('int32')
-            indices[indices == bins] = bins - 1
-            weights += ratio[indices + k * bins]
+        if self.DIMENSION == ColorHistogram.One:
+            for k in range(c):
+                indices = (np.divide(img_patch[:,:,k], 255) * bins).astype('int32')
+                indices[indices == bins] = bins - 1
+                weights += ratio[indices + k * bins]
+            weights = weights / c
 
-        return weights / c
+        if self.DIMENSION == ColorHistogram.Two:
+            hsv = cv2.cvtColor(img_patch, cv2.COLOR_BGR2HSV)
+            h, s = (hsv[:,:,0] / 180 * bins).astype('int'), (hsv[:,:,1] / 255 * bins).astype('int')
+            h[h == bins] = bins - 1
+            s[s == bins] = bins - 1
+            weights += ratio[h, s]
+
+        return weights
 
     def compute_meanshift_vector(self, img_patch, weights):
         h, w, c = img_patch.shape
@@ -154,14 +182,23 @@ class MeanShiftTracker:
 
     @staticmethod
     def compute_bhattacharyya_coef(p, q):
-        return sum(np.sqrt(p * q))
+        return np.sum(np.sqrt(p * q))
 
     @jit(cache=True)
     def color_distribution(self, img_patch):
         bins = self.bins
         h, w, c = img_patch.shape
-        # cd 即为颜色直方图特征向量，如果 c = 3，那么 cd 为 bins * 3，也就是将 rgb 颜色向量直接拼接
-        cd = np.zeros(bins * c)
+
+        if self.DIMENSION == ColorHistogram.One:
+            # 如果使用一维的颜色直方图特征
+            # cd 即为颜色直方图特征向量，如果 c = 3，那么 cd 为 bins * 3，也就是将 rgb 颜色向量直接拼接
+            cd = np.zeros(bins * c)
+        else:
+            # 如果使用二维的颜色直方图特制的
+            # cd 应该为一个 (bins, bins) 大小的矩阵，并且第一维是 hue 特征，第二维是 saturation 特征
+            cd = np.zeros((bins, bins))
+            img_patch = cv2.cvtColor(img_patch, cv2.COLOR_BGR2HSV)
+
         center = np.round(np.array([w / 2, h / 2]))
         dist = np.zeros((h, w))
 
@@ -174,13 +211,11 @@ class MeanShiftTracker:
         # normalize the distances
         dist = dist / np.max(dist)
 
+        # 下面把 1-dimension 和 2-dimension 区分开是为了方便 @jit 进行加速处理
         # build the histogram and weight with the kernel
-        for i in range(h):
-            for j in range(w):
-                for k in range(c):
-                    # compute which bin is corresponding to the pixel value
-                    index = np.int(img_patch[i, j, k] / 255 * bins)
-                    index = index - 1 if index == 8 else index
+        if self.DIMENSION == ColorHistogram.One:
+            for i in range(h):
+                for j in range(w):
                     # use Epachnikov kernel function
                     # 如果 ||x|| <= 1, k(x) = 1 - x
                     # 如果 ||x|| > 1, k(x) = 0
@@ -188,8 +223,30 @@ class MeanShiftTracker:
                         kE = 2 / np.pi * (1 - dist[i, j] ** 2)
                     else:
                         kE = 0
-                    # add the kernel value to the bin
-                    cd[k * bins + index] += kE
+
+                    for k in range(c):
+                        # compute which bin is corresponding to the pixel value
+                        index = np.int(img_patch[i, j, k] / 255 * bins)
+                        index = index - 1 if index == bins else index
+
+                        # add the kernel value to the bin
+                        cd[k * bins + index] += kE
+
+        if self.DIMENSION == ColorHistogram.Two:
+            for i in range(h):
+                for j in range(w):
+                    if dist[i, j] ** 2 < 1:
+                        kE = 2 / np.pi * (1 - dist[i, j] ** 2)
+                    else:
+                        kE = 0
+                    # 如果使用二维的颜色直方图特征，也就是一个 hue-saturation 矩阵
+                    # img_patch[i,j,0] 表示 hsv 中的 hue 分量，img_patch[i,j,1] 表示 hsv 中的 saturation 分量
+                    h_index = np.int(img_patch[i, j, 0] / 180 * bins)
+                    s_index = np.int(img_patch[i, j, 1] / 255 * bins)
+                    h_index = h_index - 1 if h_index == bins else h_index
+                    s_index = s_index - 1 if s_index == bins else s_index
+
+                    cd[h_index, s_index] += kE
 
         # normalize the kernel value
         cd = cd / np.sum(cd)
@@ -246,7 +303,7 @@ if __name__ == '__main__':
             cv2.rectangle(frame, (ix, iy), (ix + w, iy + h), (0, 255, 255), 2)
             init_tracking = False
             on_tracking = True
-            tracker.init(frame, np.array([ix, iy, w, h]))
+            tracker.init(frame, np.array([ix, iy, w, h]), ColorHistogram.Two)
         elif on_tracking:
             t0 = time()
             bounding_box = tracker.track(frame)
