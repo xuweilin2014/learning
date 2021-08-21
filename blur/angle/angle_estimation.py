e6a3d05122d7b58fa0c96655b263c1145c563f39
xuweilin <xuweilin2014@outlook.com>
2021-08-21 18:06:09 +0800

    使用 hue-saturation 作为特征

--- blur/angle/angle_estimation.py
+++ blur/angle/angle_estimation.py
@@ -7,9 +7,11 @@ from scipy.ndimage import zoom
 from skimage.transform import radon
 from sklearn.metrics import mean_squared_error
 from skimage.filters import window
-from statsmodels.graphics import tsaplots
-import librosa, librosa.display
+from blur_kernel.blur import generate_blur_kernel
+import numba
 
+EPSILON = 1.19209e-09
+BINS = 30
 
 # 将图像转换到频域
 def convert_frequency(img):
@@ -102,7 +104,7 @@ def discrete_radon_transform(image):
 
 # 使用三阶函数来对 radon 变换的结果进行拟合
 # 在运动模糊的方向，拟合结果与 radon 变换结果的 MSE 误差最大
-def poly_fit(image):
+def poly_fit_angle(image):
     image = zoom(image, 0.4)
 
     # 对图像 image 进行 radon 变换，得到 [distance, 180]
@@ -126,14 +128,96 @@ def poly_fit(image):
         # 计算 MSE 误差
         errors[i] = mean_squared_error(projection, y_vals)
 
-    thetas = np.linspace(0, theta_len, theta_len)
-    plt.plot(thetas, errors)
-    plt.title('residual figure')
-    plt.show()
+    # thetas = np.linspace(0, theta_len, theta_len)
+    # plt.plot(thetas, errors)
+    # plt.title('residual figure')
+    # plt.show()
+    return np.argmax(errors)
+
+@numba.jit(cache=True)
+def mean_shift_length(img, angle, original_img):
+    if len(img.shape) == 2:
+        img = np.reshape(img, img.shape + (1,))
+
+    h, w, _ = img.shape
+    length = np.int(np.sqrt(np.power(h, 2) + np.power(w, 2)) / 2)
+    # 线性搜索的步长默认为 4
+    step = 7
+
+    # 计算目标模型的颜色直方图特征
+    target_model = color_distribution(img)
+    max_coef = 0
+    blur_len = 1
+
+    # 模糊的长度就可以通过线性搜索的方式来获取, 步长为 step
+    for k in range(1, length + 1, step):
+        # 依据不同的模糊长度 len 产生不同的模糊核 kernel
+        kernel, anchor = generate_blur_kernel(k, angle)
+        motion_blur = cv2.filter2D(original_img, -1, kernel, anchor=anchor)
+        color_model = color_distribution(motion_blur)
+        # 计算当前模糊长度生成的图片的颜色直方图模型与目标模型的相似度
+        coef = compute_bhattacharyya_coef(target_model, color_model)
+        if coef > max_coef and k != 1:
+            blur_len = k
+            max_coef = coef
+
+    return blur_len
+
+def compute_bhattacharyya_coef(p, q):
+    return np.sum(np.sqrt(p * q))
+
+@numba.jit(cache=True)
+def color_distribution(img_patch):
+    bins = BINS
+    h, w, c = img_patch.shape
+
+    # 如果使用一维的颜色直方图特征
+    # cd 即为颜色直方图特征向量，如果 c = 3，那么 cd 为 bins * 3，也就是将 rgb 颜色向量直接拼接
+    cd = np.zeros((bins, bins))
+    img_patch = cv2.cvtColor(img_patch, cv2.COLOR_BGR2HSV)
+
+    center = np.round(np.array([w / 2, h / 2]))
+    dist = np.zeros((h, w))
+
+    # compute distances
+    # 计算 patch 中各点到 patch 中心点之间的距离
+    xv, yv = np.linspace(1, w, w), np.linspace(1, h, h)
+    x, y = np.meshgrid(xv, yv)
+    dist = np.sqrt(np.power(x - center[0], 2) + np.power(y - center[1], 2))
+
+    # normalize the distances
+    dist = dist / np.max(dist)
+
+    # 下面把 1-dimension 和 2-dimension 区分开是为了方便 @jit 进行加速处理
+    # build the histogram and weight with the kernel
+    for i in range(h):
+        for j in range(w):
+            d = dist[i, j] * dist[i, j]
+            if d < 1:
+                kE = 2 / np.pi * (1 - d)
+            else:
+                kE = 0
+            # 如果使用二维的颜色直方图特征，也就是一个 hue-saturation 矩阵
+            # img_patch[i,j,0] 表示 hsv 中的 hue 分量，img_patch[i,j,1] 表示 hsv 中的 saturation 分量
+            h_index = np.int(img_patch[i, j, 0] / 180 * bins)
+            s_index = np.int(img_patch[i, j, 1] / 255 * bins)
+            h_index = h_index - 1 if h_index == bins else h_index
+            s_index = s_index - 1 if s_index == bins else s_index
+
+            cd[h_index, s_index] += kE
+
+    # normalize the kernel value
+    cd = cd / np.sum(cd)
+    cd[cd <= EPSILON] = EPSILON
+
+    return cd
 
 if __name__ == '__main__':
     # 直接读为灰度图像
-    img = cv2.imread('imgs/cameraman_60.jpg', 0)
+    original_img = cv2.imread('imgs/peppers.jpg')
+    img_bgr = cv2.imread('imgs/peppers_60.jpg')
+    cv2.imshow('bgr', img_bgr)
+    img = cv2.imread('imgs/peppers_60.jpg', 0)
     # 在时域对图像加上汉宁窗，否则在计算 fft 过程中，由于周期延拓造成图像边缘像素的不连续，
     # 在图像的频谱图中会产生亮十字线，影响到对运动角度的检测
     img = img * window('hann', img.shape)
@@ -145,4 +229,6 @@ if __name__ == '__main__':
     img_f = np.log(np.abs(fshift))
     img_f = img_f.astype(np.uint8)
     img_f = cv2.medianBlur(img_f, 3)
-    discrete_radon_transform(img_f)
+    angle = poly_fit_angle(img_f)
+    len = mean_shift_length(img_bgr, angle, original_img)
+    print(angle, len)
